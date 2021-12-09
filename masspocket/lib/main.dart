9535da2e47a0f2e4c6fbd99caf76d8626c601403
ShadowAlzazel <pastenalfredojr@gmail.com>
2021-12-08 23:35:25 -0800

    update

--- masspocket/lib/main.dart
+++ masspocket/lib/main.dart
@@ -16,13 +16,13 @@ class MyApp extends StatelessWidget {
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
+        // application has a blue toolbar. Then, without quitting the app, try  
         // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Mass-Pocket App'),
     );
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have zero recipts saved on this Device',
+              'Reciepts saved on this device',
             ),
             Text(
               '$_counter',
